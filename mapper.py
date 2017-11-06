dd690aaa84fe8f2be88c5740a803f071e5279969
ANTO I LONAPPAN <antoidicherian@gmail.com>
2017-11-06 18:29:59 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# The code is written by Dr.Suchetana Chaterjee in Matlab,This is the python version.
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import jit
