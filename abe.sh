d1a1b0ca048198ed9c06405090ee0a1084d34ea8
ANTO I LONAPPAN <antoidicherian@gmail.com>
2018-04-10 17:20:10 +0530

    Update abe.sh

--- abe.sh
+++ abe.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-No_Pro_cm=$(awk -F "=" '/pro_cm/ {print $2}' abe.ini)
 No_Pro=$(awk -F "=" '/no_of_cores/ {print $2}' abe.ini )
 OUTPUT="RunStatus"
 
@@ -18,6 +17,7 @@ exit 1
 fi
 
 echo "Running CatalogMaker"
+No_Pro_cm=$(awk -F "=" '/pro_cm/ {print $2}' abe.ini)
 mpiexec -n $No_Pro_cm python CatalogMaker_MPI.py &>> "$OUTPUT/CatalogMaker.txt"
 if [ $? != 0 ]; then
 echo " Error while running CatalogMaker_MPI.py; check Runstatus"
